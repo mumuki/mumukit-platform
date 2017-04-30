d51165b9b5c88e2eb5a0f49c446a50667fd29ddc
Franco Bulgarelli <franco@mumuki.org>
2017-04-30 15:43:03 -0300

    Adding tests

--- spec/mumukit/organization_mapping_spec.rb
+++ spec/mumukit/organization_mapping_spec.rb
@@ -1,5 +1,53 @@
 require_relative '../spec_helper'
 
 describe Mumukit::Platform::OrganizationMapping do
-  it { expect(Mumukit::Platform::OrganizationMapping.from_env).to eq Mumukit::Platform::OrganizationMapping::Subdomain }
+
+  context 'with test defaults - http://`subdomain`.sample.app.com' do
+    let(:request) { new_rack_request 'http', 'foo.sample.app.com' }
+    it { expect(Mumukit::Platform::OrganizationMapping.from_env).to eq Mumukit::Platform::OrganizationMapping::Subdomain }
+    it { expect(Mumukit::Platform.organization_name(request)).to eq 'foo' }
+  end
+
+  describe Mumukit::Platform::OrganizationMapping::Subdomain do
+    subject { Mumukit::Platform::OrganizationMapping::Subdomain }
+
+    context 'on non central' do
+      let(:request) { new_rack_request 'http', 'foo.something.com' }
+      it { expect(subject.organization_name(request, 'something.com')).to eq 'foo' }
+    end
+
+    context 'on explicit central' do
+      let(:request) { new_rack_request 'http', 'central.something.com' }
+      it { expect(subject.organization_name(request, 'something.com')).to eq 'central' }
+    end
+
+    context 'on implicit central' do
+      let(:request) { new_rack_request 'http', 'something.com' }
+      it { expect(subject.organization_name(request, 'something.com')).to eq 'central' }
+    end
+  end
+
+  describe Mumukit::Platform::OrganizationMapping::Path do
+    subject { Mumukit::Platform::OrganizationMapping::Path }
+
+    context 'on non central' do
+      let(:request) { new_rack_request 'http', 'something.com', '80', '/foo' }
+      it { expect(subject.organization_name(request, 'something.com')).to eq 'foo' }
+    end
+
+    context 'on non central with extra path' do
+      let(:request) { new_rack_request 'http', 'something.com', '80', '/foo/bar' }
+      it { expect(subject.organization_name(request, 'something.com')).to eq 'foo' }
+    end
+
+    context 'on non central with trailing slash' do
+      let(:request) { new_rack_request 'http', 'something.com', '80', '/foo/' }
+      it { expect(subject.organization_name(request, 'something.com')).to eq 'foo' }
+    end
+
+    context 'on central' do
+      let(:request) { new_rack_request 'http', 'something.com', '80', '/central/' }
+      it { expect(subject.organization_name(request, 'something.com')).to eq 'central' }
+    end
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,12 @@ RSpec.configure do |config|
   end
 end
 
+def new_rack_request(protocol, host, port = '80', path = '/')
+  Rack::Request.new 'HTTP_HOST' => host,
+                    'rack.url_scheme' => protocol,
+                    'SERVER_PORT' => port,
+                    'PATH_INFO' => path
+end
 
 Mumukit::Platform.configure do |config|
   config.application = Mumukit::Platform::Application::Organic.new 'http://sample.app.com'
