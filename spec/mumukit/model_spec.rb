3855db569405aca9e23bc4c17bacb68167f46dd1
Julián Berbel Alt <julianberbel@hotmail.com>
2018-09-20 15:44:03 -0300

    add tests to showcase problem

--- spec/mumukit/model_spec.rb
+++ spec/mumukit/model_spec.rb
@@ -7,4 +7,14 @@ describe Mumukit::Platform::Model do
 
   it { expect(Mancuspia.accessors).to eq [:name, :age, :heavy?, :name=, :age=, :heavy=] }
   it { expect(Mancuspia.attributes).to eq [:name, :age, :heavy] }
+
+  context 'if model_attr_accessor is used more than once accessors and attributes get extended' do
+    class SomeModel < Mumukit::Platform::Model
+      model_attr_accessor :name, :heavy?
+      model_attr_accessor :age, :big?
+    end
+
+    it { expect(SomeModel.accessors).to eq [:name, :heavy?, :age, :big?, :name=, :heavy=, :age=, :big=] }
+    it { expect(SomeModel.attributes).to eq [:name, :heavy, :age, :big] }
+  end
 end
