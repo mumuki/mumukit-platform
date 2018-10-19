953e1571462c39bdc6ccb6871a8be6e90f92ffe6
Franco Bulgarelli <franco@mumuki.org>
2018-10-19 16:22:15 -0300

    Removing import methods

--- spec/mumukit/bridge_spec.rb
+++ spec/mumukit/bridge_spec.rb
@@ -8,34 +8,4 @@ describe Mumukit::Platform do
   it { expect(Mumukit::Platform.bibliotheca_bridge).to be_a Mumukit::Bridge::Bibliotheca }
   it { expect(Mumukit::Platform.bibliotheca_bridge.url).to eq 'http://bibliotheca-api.localmumuki.io' }
 
-  describe 'import_contents!' do
-    let(:imported_resources) { [] }
-    let(:bridge) { Mumukit::Bridge::Bibliotheca.new('http://nonexistenurl.com') }
-    let(:append_resources) { proc { |resource_type, slug| imported_resources << [resource_type, slug] } }
-
-    before do
-      expect(bridge).to receive(:get_collection).and_return(
-        [{'slug' => 'foo/a-guide'}, {'slug' => 'baz/a-guide'}, {'slug' => 'foo/another-guide'}],
-        [{'slug' => 'foo/a-topic'}, {'slug' => 'baz/a-topic'}],
-        [])
-    end
-
-    context 'all match' do
-      before { bridge.import_contents!(&append_resources) }
-      it do
-        expect(imported_resources).to eq [
-          ['guide', 'foo/a-guide'],
-          ['guide', 'baz/a-guide'],
-          ['guide', 'foo/another-guide'],
-          ['topic', 'foo/a-topic'],
-          ['topic', 'baz/a-topic'],
-        ]
-      end
-    end
-
-    context 'some match' do
-      before { bridge.import_contents!(/^foo.*$/, &append_resources) }
-      it { expect(imported_resources).to eq [['guide', 'foo/a-guide'], ['guide', 'foo/another-guide'], ['topic', 'foo/a-topic']] }
-    end
-  end
 end
