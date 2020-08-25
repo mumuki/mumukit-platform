d8ab8d86f65d4839ed07f75f91f97c7c80d3a99d
Julián Berbel Alt <julian@mumuki.org>
2020-08-25 14:06:36 -0300

    Add chilean spanish to supported locales

--- lib/mumukit/platform/locale.rb
+++ lib/mumukit/platform/locale.rb
@@ -2,6 +2,7 @@ module Mumukit::Platform::Locale
   SPECS = {
     en: { facebook_code: :en_US, auth0_code: :en, name: 'English' },
     es: { facebook_code: :es_LA, auth0_code: :es, name: 'Español' },
+    'es-CL': { facebook_code: :es_LA, auth0_code: :es, name: 'Español chileno' },
     pt: { facebook_code: :pt_BR, auth0_code: 'pt-br', name: 'Português' }
   }.with_indifferent_access
 
